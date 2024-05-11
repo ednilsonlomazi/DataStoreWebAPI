f4472604a5c8bcf15962916605a1b49914126a8b
ednilson-lomazi <65460604+ednilsonlomazi@users.noreply.github.com>
2024-05-11 19:46:32 -0300

    rota-iniciar-avaliacao

--- DataStoreWebAPI/Controllers/TabAvaliacaoController.cs
+++ DataStoreWebAPI/Controllers/TabAvaliacaoController.cs
@@ -37,6 +37,33 @@ namespace DataStoreWebAPI.Controllers
             return NotFound();
         }
 
+        // iniciar uma avaliacao
+        [HttpGet("iniciar-avaliacao/{cod_documento}/{cod_avaliador}")]
+        public IActionResult GetDocumentosParaAvaliar(int cod_documento, int cod_avaliador)
+        {
+            var documento = this._dbContext.tabDocumento.Where(td => td.isCanceled == false 
+                                                                   && td.isOpen 
+                                                                   && td.emissor == null
+                                                                   && td.codigoDocumento == cod_documento
+                                                                ).Single();
+            if(documento != null)
+            {
+                var avaliador = this._dbContext.tabEmissors.Where(te => te.codigoEmissor == cod_avaliador).Single();
+                if(avaliador != null)
+                {
+                    documento.emissor = avaliador;
+                    this._dbContext.tabDocumento.Update(documento);
+                    this._dbContext.SaveChanges();
+                    return Ok();
+
+                }
+                return NotFound();
+                
+                
+            }
+            return NotFound();
+        }        
+
 
     }
 }
